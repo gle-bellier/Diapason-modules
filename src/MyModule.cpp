340b51ecffe84d49237874ec93f3322d3641628c
Andrew Belt <andrewpbelt@gmail.com>
2019-01-17 03:36:31 -0500

    Revise code format

--- src/MyModule.cpp
+++ src/MyModule.cpp
@@ -2,19 +2,19 @@
 
 
 struct MyModule : Module {
-	enum ParamIds {
+	enum ParamId {
 		PITCH_PARAM,
 		NUM_PARAMS
 	};
-	enum InputIds {
+	enum InputId {
 		PITCH_INPUT,
 		NUM_INPUTS
 	};
-	enum OutputIds {
+	enum OutputId {
 		SINE_OUTPUT,
 		NUM_OUTPUTS
 	};
-	enum LightIds {
+	enum LightId {
 		BLINK_LIGHT,
 		NUM_LIGHTS
 	};
@@ -27,41 +27,43 @@ struct MyModule : Module {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 
 		// Configure parameters
+		// See engine/Param.hpp for config() arguments
 		params[PITCH_PARAM].config(-3.f, 3.f, 0.f, "Pitch", " Hz", 2.f, dsp::FREQ_C4);
 	}
-	void step() override;
+
+	void step() override {
+		// Implement a simple sine oscillator
+
+		// Get timestep duration
+		float deltaTime = app()->engine->getSampleTime();
+
+		// Compute the frequency from the pitch parameter and input
+		float pitch = params[PITCH_PARAM].getValue();
+		pitch += inputs[PITCH_INPUT].getValue();
+		pitch = clamp(pitch, -4.f, 4.f);
+		// The default pitch is C4 = 261.6256
+		float freq = dsp::FREQ_C4 * std::pow(2.f, pitch);
+
+		// Accumulate the phase
+		phase += freq * deltaTime;
+		phase = std::fmod(phase, 1.f);
+
+		// Compute the sine output
+		float sine = std::sin(2.f * M_PI * phase);
+		outputs[SINE_OUTPUT].numChannels = 16;
+		outputs[SINE_OUTPUT].setVoltage(5.f * sine);
+
+		// Blink light at 1Hz
+		blinkPhase += deltaTime;
+		blinkPhase = std::fmod(blinkPhase, 1.f);
+		lights[BLINK_LIGHT].setBrightness(blinkPhase < 0.5f ? 1.f : 0.f);
+	}
 
 	// For more advanced Module features, see engine/Module.hpp in the Rack API.
 	// - dataToJson, dataFromJson: serialization of internal data
 	// - onSampleRateChange: event triggered by a change of sample rate
 	// - onReset, onRandomize: implements custom behavior requested by the user
 };
-
-
-void MyModule::step() {
-	// Implement a simple sine oscillator
-	float deltaTime = app()->engine->getSampleTime();
-
-	// Compute the frequency from the pitch parameter and input
-	float pitch = params[PITCH_PARAM].value;
-	pitch += inputs[PITCH_INPUT].value;
-	pitch = clamp(pitch, -4.f, 4.f);
-	// The default pitch is C4 = 261.6256
-	float freq = dsp::FREQ_C4 * std::pow(2.f, pitch);
-
-	// Accumulate the phase
-	phase += freq * deltaTime;
-	phase = std::fmod(phase, 1.f);
-
-	// Compute the sine output
-	float sine = std::sin(2.f * M_PI * phase);
-	outputs[SINE_OUTPUT].setVoltage(5.f * sine);
-
-	// Blink light at 1Hz
-	blinkPhase += deltaTime;
-	blinkPhase = std::fmod(phase, 1.f);
-	lights[BLINK_LIGHT].setBrightness(blinkPhase < 0.5f ? 1.f : 0.f);
-}
 
 
 struct MyModuleWidget : ModuleWidget {
