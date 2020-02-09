3358ddbbda1f14c68661bc9ad425a97b27d72511
Erik <glebellier@protonmail.com>
2020-02-09 17:39:43 +0100

    Make the script cleaner.

--- src/testPlugIn.cpp
+++ src/testPlugIn.cpp
@@ -6,52 +6,44 @@
 
 
 
-struct HPFilter2 {
-	float bX1;
-	float bX2;
-	float bY1;
-	float bY2;
-	float q;
-	double thetaC;
+struct lpFilter2 {
 
-
-
-	void setCutoff(float freq, float Tn){
-		thetaC = 2*M_PI*freq*Tn;
-	}
-	void setQ(float qFactor){
-		q = qFactor;
-	}
+	float bX1=0;
+	float bX2=0;
+	float bY1=0;
+	float bY2=0;
 
 	float process (float X0, float freq, float qFactor,float Tn){
-		setCutoff(freq,Tn);
-		setQ(qFactor);
-		double d2 = 0.5f/q;
-		double B = 0.5f*(1-d2*std::sin(thetaC))/(1+d2*std::sin(thetaC));
-		double A = (0.5f + B)*std::cos(thetaC);
+			//setCutoff(freq,Tn);
+			float thetaC = 2*M_PI*freq*Tn;
+			std::cout << "Freq = " << freq << '\n';
+			float q = qFactor;
+			float d = 1.f/q;
+			float fBetaNumerator = 1.f - 0.5f*d*std::sin(thetaC);
+			float fBetaDenominator = 1.f + 0.5f*d*std::sin(thetaC);
+			float fBeta = 0.5f*(fBetaNumerator/fBetaDenominator);
+			float fGamma = (0.5f + fBeta)*std::cos(thetaC);
+			float fAlpha = (0.5f + fBeta - fGamma)/2.0f;
 
-		float a0 = (0.5 + B + A)/2.0f;
-		float a1 = -2.f*a0;
-		float a2 = a0;
-		float b1 = -2.f*A;
-		float b2 = 2.f*B;
-		// double a0 = (0.5f + B - A)/2.0f;
-		// double a1 = 2.f*a0;
-		// double a2 = a0;
-		// double b1 = -2.f*A;
-		// double b2 = 2.f*B;
+			float coeff0 = fAlpha;
+			float coeff1 = 2.f*fAlpha;
+			float coeff2 = fAlpha;
+			float coeff3 = -2.f*fGamma;
+			float coeff4 = 2.f*fBeta;
 
-		double output = a0*X0 + a1*bX1 + a2*bX2 - b1*bY1 - b2*bY2;
-		bX1 = X0;
-		bX2 = bX1;
-		bY1 = output;
-		bY2 = bY1;
-		return output;
-	}
+			std::cout << "Input = "<< X0 << '\n';
+			float output = coeff0*X0 + coeff1*bX1 + coeff2*bX2 - coeff3*bY1 - coeff4*bY2;
+			//float output = coeff0*X0 + coeff1*buffers[0] + coeff2*buffers[1] - coeff3*buffers[2] - coeff4*buffers[3];
+			std::cout << "Output = " << output << '\n';
+			// bufferize(X0,output);
+			bX2=bX1;
+			bX1=X0;
+			bY2=bY1;
+			bY1=output;
 
+			return output;
+		}
 };
-
-
 
 
 struct Test : Module {
@@ -65,7 +57,8 @@ struct Test : Module {
 		NUM_INPUTS
 	};
 	enum OutputId {
-		OUTPUT,
+		OUTPUTLP,
+		OUTPUTHP,
 		NUM_OUTPUTS
 	};
 	enum LightId {
@@ -73,61 +66,23 @@ struct Test : Module {
 		NUM_LIGHTS
 	};
 	float phase = 0.f;
-	// float bX1 = 0;
-	// float bX2 = 0;
-	// float bY1 = 0;
-	// float bY2 = 0;
-	HPFilter2 filter;
+	lpFilter2 filterlp;
 
 	Test() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(FREQUENCY, 0.f, 10000.f,2000.f, "Amount", "");
-		configParam(QFACTOR, 0.1f, 10.f, 5.f, "Amount", "");
+		configParam(FREQUENCY, 0.f, 1.f, 0.5f, "Frequency", " Hz", std::pow(2, 10.f), dsp::FREQ_C4 / std::pow(2, 5.f));
+		configParam(QFACTOR, 0.01f, 1.f, 0.5f, "Amount", "");
 	}
 	void process(const ProcessArgs &args) override {
 		// get value of parameter
-		float q = params[QFACTOR].getValue();
-		float freq = params[FREQUENCY].getValue();
-		std::cout<< freq;
-
-		// double thetaC = 2*M_PI*freq*args.sampleTime;
-		// double d2 = 0.5f/q;
-		//
-		// // float gamma = std::cos(thetaC)/(1.f+std::sin(thetaC));
-		// //
-		// // float a0 = (1.f - gamma)/2.0f;
-		// // float a1 = a0;
-		// // float a2 = 0.f;
-		// // float b1 = -gamma;
-		// // float b2 = 0.f;
-		//
-		// double B = 0.5f*(1-d2*std::sin(thetaC))/(1+d2*std::sin(thetaC));
-		// double A = (0.5f + B)*std::cos(thetaC);
-		//
-		//
-		// double a0 = (0.5f + B - A)/2.0f;
-		// double a1 = 2.f*a0;
-		// double a2 = a0;
-		// double b1 = -2.f*A;
-		// double b2 = 2.f*B;
-		//
-		//
-		// /* HPF
-		// float a0 = (0.5 + B + A)/2.0f;
-		// float a1 = -2.f*a0;
-		// float a2 = a0;
-		// float b1 = -2.f*A;
-		// float b2 = 2.f*B;
-		// */
-		double X0 = inputs[INPUT].getVoltage();
-		// double output = a0*X0 + a1*bX1 + a2*bX2 - b1*bY1 - b2*bY2;
-		//
-		// bX1 = X0;
-		// bX2 = bX1;
-		// bY1 = output;
-		// bY2 = bY1;
-		float output = filter.process(X0,freq,q,args.sampleTime);
-		outputs[OUTPUT].setVoltage(simd::clamp(output,-5.f,5.f));
+		float resonance = params[QFACTOR].getValue();
+		float q = simd::pow(resonance, 2) * 10.f;
+		float pitch = params[FREQUENCY].getValue();
+		pitch = pitch * 10.f - 5.f;
+		float freq = dsp::FREQ_C4 * simd::pow(2.f, pitch);
+		float X0 = inputs[INPUT].getVoltage() + 1e-6f * (2.f * random::uniform() - 1.f);;
+		float outputlp = filterlp.process(X0,freq,q,args.sampleTime);
+		outputs[OUTPUTLP].setVoltage(simd::clamp(outputlp,-5.f,5.f));
 	}
 };
 
@@ -148,7 +103,7 @@ struct TestWidget : ModuleWidget {
 		addParam(createParam<RoundHugeBlackKnob>(Vec(49, 155), module, Test::QFACTOR));
 
 		addInput(createInput<PJ301MPort>(Vec(28, 320), module, Test::INPUT));
-		addOutput(createOutput<PJ301MPort>(Vec(98, 320), module, Test::OUTPUT));
+		addOutput(createOutput<PJ301MPort>(Vec(68, 320), module, Test::OUTPUTLP));
 
 		//addChild(createLight<MediumLight<RedLight>>(Vec(41, 59), module, Test::BLINK_LIGHT));
 	}
