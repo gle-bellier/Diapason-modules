5a7475efd79ec6b5dc75061d897535fa586c13fc
Erik <glebellier@protonmail.com>
2020-02-10 14:44:40 +0100

    Add Band-Pass and Band-stop Filters.

--- src/testPlugIn.cpp
+++ src/testPlugIn.cpp
@@ -6,7 +6,7 @@
 
 
 
-class filter2 {
+class filter {
 	protected :
 	float buffers[4]; // Order : X1,X2,Y1,Y2
 	float coeffs[5];// order : a0, a1, a2, b1, b2
@@ -14,23 +14,20 @@ class filter2 {
 	void bufferize(float, float);
 	void setCutoff(float,float);
 };
-void filter2::bufferize(float in, float out){
+void filter::bufferize(float in, float out){
 	buffers[1]=buffers[0];
 	buffers[0]=in;
 	buffers[3]=buffers[2];
 	buffers[2]=out;
 }
-void filter2::setCutoff(float frequency, float tSampling){
+void filter::setCutoff(float frequency, float tSampling){
 		thetaC = 2*M_PI*frequency*tSampling;
 }
 
-
-
-class lpFilter2 : public filter2{
+class lpFilter2 : public filter{
 	public :
 	float process(float, float, float, float);
 };
-
 float lpFilter2::process (float X0, float freq, float qFactor,float Tn){
 			setCutoff(freq,Tn);
 			float d = 1.f/qFactor;
@@ -49,11 +46,10 @@ float lpFilter2::process (float X0, float freq, float qFactor,float Tn){
 			return output;
 		}
 
-class hpFilter2 : public filter2{
+class hpFilter2 : public filter{
 	public :
 	float process(float, float, float, float);
 };
-
 float hpFilter2::process (float X0, float freq, float qFactor,float Tn){
 			setCutoff(freq,Tn);
 			float d = 1.f/qFactor;
@@ -72,6 +68,47 @@ float hpFilter2::process (float X0, float freq, float qFactor,float Tn){
 			return output;
 		}
 
+class bpFilter2 : public filter{
+	public :
+	float process(float, float, float, float);
+};
+float bpFilter2::process (float X0, float freq, float qFactor,float Tn){
+			setCutoff(freq,Tn);
+			float fBetaNumerator = 1.f - std::tan(simd::clamp(thetaC/(2.f*qFactor),0.f,3.14f/2.f));
+			float fBetaDenominator = 1.f + std::tan(simd::clamp(thetaC/(2.f*qFactor),0.f,3.14f/2.f));
+			float fBeta = 0.5f*(fBetaNumerator/fBetaDenominator);
+			float fGamma = (0.5f + fBeta)*std::cos(thetaC);
+			float fAlpha = 0.5f - fBeta;
+			coeffs[0] = fAlpha;
+			coeffs[1] = 0.f;
+			coeffs[2] = -fAlpha;
+			coeffs[3] = -2.f*fGamma;
+			coeffs[4] = 2.f*fBeta;
+			float output = coeffs[0]*X0 + coeffs[1]*buffers[0] + coeffs[2]*buffers[1] - coeffs[3]*buffers[2] - coeffs[4]*buffers[3];
+			bufferize(X0,output);
+			return output;
+		}
+
+class bsFilter2 : public filter{
+	public :
+	float process(float, float, float, float);
+};
+float bsFilter2::process (float X0, float freq, float qFactor,float Tn){
+			setCutoff(freq,Tn);
+			float fBetaNumerator = 1.f - std::tan(simd::clamp(thetaC/(2.f*qFactor),0.f,3.14f/2.f));
+			float fBetaDenominator = 1.f + std::tan(simd::clamp(thetaC/(2.f*qFactor),0.f,3.14f/2.f));
+			float fBeta = 0.5f*(fBetaNumerator/fBetaDenominator);
+			float fGamma = (0.5f + fBeta)*std::cos(thetaC);
+			float fAlpha = 0.5f + fBeta;
+			coeffs[0] = fAlpha;
+			coeffs[1] = -2.f*fGamma;
+			coeffs[2] = fAlpha;
+			coeffs[3] = -2.f*fGamma;
+			coeffs[4] = 2.f*fBeta;
+			float output = coeffs[0]*X0 + coeffs[1]*buffers[0] + coeffs[2]*buffers[1] - coeffs[3]*buffers[2] - coeffs[4]*buffers[3];
+			bufferize(X0,output);
+			return output;
+		}
 
 
 struct Test : Module {
@@ -94,8 +131,8 @@ struct Test : Module {
 		NUM_LIGHTS
 	};
 	float phase = 0.f;
-	lpFilter2 filterlp;
-	hpFilter2 filterhp;
+	bpFilter2 filterlp;
+	bsFilter2 filterhp;
 	Test() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(FREQUENCY, 0.f, 1.f, 0.5f, "Frequency", " Hz", std::pow(2, 10.f), dsp::FREQ_C4 / std::pow(2, 5.f));
